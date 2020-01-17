fb716890274c2a411ccc6b1eacdf96e54eddef9a
Locketgoma <gom@kiwing.co.kr>
2020-01-18 07:12:30 +0900

    기본 네트워크 세팅

--- Connection.cpp
+++ Connection.cpp
@@ -1,1 +1,15 @@
 #include "Connection.h"
+#include <cassert>
+#include <iostream>
+
+
+SimpleSocket::SimpleSocket() {
+	assert(WSAStartup(MAKEWORD(2, 2), &wsaData) == 0);
+		std::cout << "Error!" << std::endl;
+		
+
+	hBSock = socket(PF_INET, SOCK_STREAM, 0);
+	hRSock = 0;
+	
+	
+}
--- Connection.h
+++ Connection.h
@@ -9,3 +9,38 @@
 
 //실제 "환경 대응"식 작업시에는, unix 기반에서는 sys/socket.h, windows 기반에서는 winsock를 사용하면 된다.
 // = 두 환경을 비교, 대응할 수 있는 방식으로 작성해야 할 것
+
+
+class SimpleSocket {
+	public:
+		//SOCKET = uint
+		SOCKET hBSock;		//'Server' 포지션
+		SOCKET hRSock;		//'Client' 포지션
+		SOCKADDR_IN hBAddr; //'Server' 포지션
+		SOCKADDR_IN hRAddr; //'Client' 포지션
+
+		int portnumb;		//기본 포트번호
+
+		SimpleSocket();
+
+		bool setOppositIP();
+		bool setPortNumb();
+
+	private:
+		WSADATA wsaData;
+
+
+		bool initSocket();		//초기정보 가공
+
+
+};
+
+/*
+struct sockaddr_in {
+		short   sin_family;
+		u_short sin_port;
+		struct  in_addr sin_addr;
+		char    sin_zero[8];
+};
+
+*/
